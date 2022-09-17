54941f7af74e2e5935e8910028cfdfb73d48bab6
KadaHarshavardhan <kadaharshavardhan@gmail.com>
2022-09-17 15:37:28 +0530

    Initial commit

---
+++ Sources/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 0;
+    int b = 0;
+    printf("Enter values for adder: %d\n");
+    scanf("%d\n", &a);
+    scanf("%d\n", &a);
+    printf("Adder result: %d\n", adder(a, b));
+}
